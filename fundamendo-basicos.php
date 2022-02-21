95f78c02b7c66b3a438e0517862708883b58a79b
Andre-Goncalves-Martins <andregmartins26@gmail.com>
2022-02-21 15:15:32 -0300

    Como imprimir algo na tela com PHP

---
+++ fundamendo-basicos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Fundamentos Básicos</title>
+</head>
+<body>
+    
+<?php
+//comentario de linha
+/*
+    Primeiros passos com PHP
+*/
+    echo "Meu nome é André <br>";
+    print "Minha idade é 22";
+?>
+
+</body>
